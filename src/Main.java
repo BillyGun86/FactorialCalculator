bfbaa27522cff8b98bc5ff96fb08f540428fde3b
BillyGun86 <billysam3@gmail.com>
2022-06-21 20:57:44 +0400

    init

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        int result = 1;
+        int value = new Scanner(System.in).nextInt();
+        for (int i = 1; i <= value; i++) {
+            result = i * result;
+        }
+
+        System.out.println(result);
+    }
 }
